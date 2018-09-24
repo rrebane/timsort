cee2c09b44b95f232707a8890afa16502b61219d
Reimo Rebane <reimo.rebane@cyber.ee>
2018-09-24 21:05:39 +0300

    Refactored benchmark code.

--- test/benchmark.cpp
+++ test/benchmark.cpp
@@ -1,173 +1,74 @@
-#include <algorithm>
 #include <benchmark/benchmark.h>
 #include <random>
 #include <vector>
 
 #include <timsort/timsort.h>
 
-template <typename Container,
-          typename T = typename Container::value_type,
-          typename = typename std::enable_if<std::is_integral<T>::value>::type>
-void random_fill(std::random_device & rd, Container & values) {
-    std::default_random_engine re(rd());
-    std::uniform_int_distribution<T> dist;
+namespace {
 
-    for (auto & val : values) {
-        val = dist(re);
-    }
-}
+std::random_device gRandom;
 
-static void timsort_bench_asc(benchmark::State & state) {
-    std::size_t bytes = state.range(0);
-    std::size_t size = bytes / sizeof(int);
+} // namespace
 
-    std::random_device rd;
-    std::vector<int> values;
-    for (std::size_t i = 0u; i < size; ++i) {
-        values.emplace_back(static_cast<int>(i));
-    }
-
-    for (auto _ : state) {
-        timsort::sort(values.begin(), values.end());
-    }
-
-    state.SetBytesProcessed(state.iterations() * bytes);
-}
-
-BENCHMARK(timsort_bench_asc)->Range(8, 8 << 19);
-
-static void timsort_bench_desc(benchmark::State & state) {
-    std::size_t bytes = state.range(0);
-    std::size_t size = bytes / sizeof(int);
-
-    std::random_device rd;
-    std::vector<int> values;
-    values.resize(size);
-
-    for (auto _ : state) {
-        state.PauseTiming();
-        assert(values.size() == size);
-        for (std::size_t i = 0u; i < size; ++i) {
-            values[i] = static_cast<int>(size - i - 1u);
+class AscGen {
+public:
+    template <typename Container, typename T = typename Container::value_type>
+    void operator()(Container & c) {
+        T value = 0;
+        for (auto & val : c) {
+            val = value++;
         }
-        state.ResumeTiming();
-        timsort::sort(values.begin(), values.end());
     }
+};
 
-    state.SetBytesProcessed(state.iterations() * bytes);
-}
-
-BENCHMARK(timsort_bench_desc)->Range(8, 8 << 19);
-
-static void timsort_bench_eq(benchmark::State & state) {
-    std::size_t bytes = state.range(0);
-    std::size_t size = bytes / sizeof(int);
-
-    std::random_device rd;
-    std::vector<int> values;
-    values.resize(size);
-    std::fill(values.begin(), values.end(), 1);
-
-    for (auto _ : state) {
-        timsort::sort(values.begin(), values.end());
-    }
-
-    state.SetBytesProcessed(state.iterations() * bytes);
-}
-
-BENCHMARK(timsort_bench_eq)->Range(8, 8 << 19);
-
-static void timsort_bench_random(benchmark::State & state) {
-    std::size_t bytes = state.range(0);
-    std::size_t size = bytes / sizeof(int);
-
-    std::random_device rd;
-    std::vector<int> values;
-    values.resize(size);
-
-    for (auto _ : state) {
-        state.PauseTiming();
-        random_fill(rd, values);
-        state.ResumeTiming();
-        timsort::sort(values.begin(), values.end());
-    }
-
-    state.SetBytesProcessed(state.iterations() * bytes);
-}
-
-BENCHMARK(timsort_bench_random)->Range(8, 8 << 19);
-
-static void stdsort_bench_asc(benchmark::State & state) {
-    std::size_t bytes = state.range(0);
-    std::size_t size = bytes / sizeof(int);
-
-    std::random_device rd;
-    std::vector<int> values;
-    for (std::size_t i = 0u; i < size; ++i) {
-        values.emplace_back(static_cast<int>(i));
-    }
-
-    for (auto _ : state) {
-        std::stable_sort(values.begin(), values.end());
-    }
-
-    state.SetBytesProcessed(state.iterations() * bytes);
-}
-
-BENCHMARK(stdsort_bench_asc)->Range(8, 8 << 19);
-
-static void stdsort_bench_desc(benchmark::State & state) {
-    std::size_t bytes = state.range(0);
-    std::size_t size = bytes / sizeof(int);
-
-    std::random_device rd;
-    std::vector<int> values;
-    values.resize(size);
-
-    for (auto _ : state) {
-        state.PauseTiming();
-        assert(values.size() == size);
-        for (std::size_t i = 0u; i < size; ++i) {
-            values[i] = static_cast<int>(size - i - 1u);
+class DescGen {
+public:
+    template <typename Container, typename T = typename Container::value_type>
+    void operator()(Container & c) {
+        T value = c.size();
+        for (auto & val : c) {
+            val = --value;
         }
-        state.ResumeTiming();
-        std::stable_sort(values.begin(), values.end());
+    }
+};
+
+class EqGen {
+public:
+    template <typename Container, typename T = typename Container::value_type>
+    void operator()(Container & c) {
+        for (auto & val : c) {
+            val = 0;
+        }
+    }
+};
+
+class RandomGen {
+public:
+    template <typename Container, typename T = typename Container::value_type,
+              typename = typename std::enable_if<std::is_integral<T>::value>::type>
+    void operator()(Container & c) {
+        std::uniform_int_distribution<T> distributon;
+        for (auto & val : c) {
+            val = distributon(m_engine);
+        }
     }
 
-    state.SetBytesProcessed(state.iterations() * bytes);
-}
+private:
+    std::default_random_engine m_engine = std::default_random_engine{gRandom()};
+};
 
-BENCHMARK(stdsort_bench_desc)->Range(8, 8 << 19);
-
-static void stdsort_bench_eq(benchmark::State & state) {
+template <class Container, class Generator>
+void stdsort_bench(benchmark::State & state) {
     std::size_t bytes = state.range(0);
-    std::size_t size = bytes / sizeof(int);
+    std::size_t size = bytes / sizeof(typename Container::value_type);
 
-    std::random_device rd;
-    std::vector<int> values;
-    values.resize(size);
-    std::fill(values.begin(), values.end(), 1);
-
-    for (auto _ : state) {
-        std::stable_sort(values.begin(), values.end());
-    }
-
-    state.SetBytesProcessed(state.iterations() * bytes);
-}
-
-BENCHMARK(stdsort_bench_eq)->Range(8, 8 << 19);
-
-static void stdsort_bench_random(benchmark::State & state) {
-    std::size_t bytes = state.range(0);
-    std::size_t size = bytes / sizeof(int);
-
-    std::random_device rd;
-    std::vector<int> values;
+    Container values;
+    Generator generator;
     values.resize(size);
 
     for (auto _ : state) {
         state.PauseTiming();
-        random_fill(rd, values);
+        generator(values);
         state.ResumeTiming();
         std::stable_sort(values.begin(), values.end());
     }
@@ -175,6 +76,32 @@ static void stdsort_bench_random(benchmark::State & state) {
     state.SetBytesProcessed(state.iterations() * bytes);
 }
 
-BENCHMARK(stdsort_bench_random)->Range(8, 8 << 19);
+template <class Container, class Generator>
+void timsort_bench(benchmark::State & state) {
+    std::size_t bytes = state.range(0);
+    std::size_t size = bytes / sizeof(typename Container::value_type);
+
+    Container values;
+    Generator generator;
+    values.resize(size);
+
+    for (auto _ : state) {
+        state.PauseTiming();
+        generator(values);
+        state.ResumeTiming();
+        timsort::sort(values.begin(), values.end());
+    }
+
+    state.SetBytesProcessed(state.iterations() * bytes);
+}
+
+BENCHMARK_TEMPLATE(timsort_bench, std::vector<int>, RandomGen)->Range(8, 8 << 19);
+BENCHMARK_TEMPLATE(stdsort_bench, std::vector<int>, RandomGen)->Range(8, 8 << 19);
+BENCHMARK_TEMPLATE(timsort_bench, std::vector<int>, AscGen)->Range(8, 8 << 19);
+BENCHMARK_TEMPLATE(stdsort_bench, std::vector<int>, AscGen)->Range(8, 8 << 19);
+BENCHMARK_TEMPLATE(timsort_bench, std::vector<int>, DescGen)->Range(8, 8 << 19);
+BENCHMARK_TEMPLATE(stdsort_bench, std::vector<int>, DescGen)->Range(8, 8 << 19);
+BENCHMARK_TEMPLATE(timsort_bench, std::vector<int>, EqGen)->Range(8, 8 << 19);
+BENCHMARK_TEMPLATE(stdsort_bench, std::vector<int>, EqGen)->Range(8, 8 << 19);
 
 BENCHMARK_MAIN();
